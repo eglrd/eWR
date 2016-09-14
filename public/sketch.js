b7913568b45c7dab62c0754ecf24d7c9361780c1
eglrd <etgaillard@gmail.com>
2016-09-15 00:41:19 +0200

    playing with lego blog from many great dudes

--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,16 @@
+var socket;
 
 function setup() {
   createCanvas(320, 240);
   background(51);
+
+  socket = io.connect('http://localhost:3000');
 }
+
+//https://www.youtube.com/watch?v=HZWmrt3Jy10 @ 7.31
 
 function draw() {
   s = "hello world";
-  fill(50);
+  fill(255);
   text(s, 10, 10, 70, 80);
 }
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+// this is a node.js based webserver (or webservice ?) experimenting with waterrower interfacing via sockets..
 var waterrower = require("Waterrower");
 var express = require('express');
+var socket = require('socket.io');
 
+// start webserver and set default directory
 var app = express();
 var server = app.listen(3000);
 app.use(express.static('public'));
+
+//setup socket connections (sever side)
+var io = socket(server);
+//function to handle connections
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+  console.log(socket);
+}
+
 
 var readWaterrower = function() {
   console.log();
