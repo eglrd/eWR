92b5a7e78d97be2f757a415615623f645cd9cd1c
eglrd <etgaillard@gmail.com>
2016-09-15 22:41:17 +0200

    added watts - note to self to contribute back

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ function newConnection(socket){
 
 var readWaterrower = function() {
   console.log();
-  console.log("Stroke Rate ....." + waterrower.readStrokeCount());  // [ - ]
-  console.log("Total Speed ....." + waterrower.readTotalSpeed());   // [cm/s]
-  console.log("Average Speed ..." + waterrower.readAverageSpeed()); // [cm/s]
-  console.log("Distance... ....." + waterrower.readDistance());     // [ m ]
+  // console.log("Stroke Rate ....." + waterrower.readStrokeCount());  // [ - ]
+  // console.log("Total Speed ....." + waterrower.readTotalSpeed());   // [cm/s]
+  // console.log("Average Speed ..." + waterrower.readAverageSpeed()); // [cm/s]
+  // console.log("Distance... ....." + waterrower.readDistance());     // [ m ]
+  console.log("Watts ..........." + waterrower.readWatts());    // [ bpm ]
   console.log("Heart Rate ......" + waterrower.readHeartRate());    // [ bpm ]
 
 }
