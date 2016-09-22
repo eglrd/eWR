23bffd933d9c6787aa381be52aa86e5e7cab2b64
eglrd <etgaillard@gmail.com>
2016-09-22 22:31:33 +0200

    rebuilt waterrower read/write

--- public/sketch.js
+++ public/sketch.js
@@ -15,34 +15,26 @@ function setup() {
   });
 }
 
-// function mousePressed(){
-//   socket.emit('click', mouseX+" "+mouseY);
-// }
-
-
 
 //https://www.youtube.com/watch?v=HZWmrt3Jy10 @ 7.31
 
 function draw() {
   background(51);
   if (dataReceived){
-      testText = "Stroke Count: " + dataReceived.logData.sCount + " Strokes\n";
-      testText = "Stroke Rate: " + dataReceived.logData.sRate + " S/mn\n";
-      testText = testText + "Total Speed: " + dataReceived.logData.tSpeed + " cm/s \n";
-      testText = testText + "Average Speed: " + dataReceived.logData.aSpeed + " cm/s \n";
-      testText = testText + "Distance: " + dataReceived.logData.d + " m \n";
-      testText = testText + "Watts: " + dataReceived.logData.w + " w \n";
-      testText = testText + "HR: " + dataReceived.logData.hR + " bpm \n";
+      testText = "Stroke Count: " + dataReceived.totalStroke + " Strokes\n";
+      testText = "Stroke Rate: " + dataReceived.strokeRate + " S/mn\n";
+      testText = testText + "Instant Speed: " + dataReceived.iSpeed + " cm/s \n";
+      testText = testText + "Average Speed: " + dataReceived.aSpeed + " cm/s \n";
+      testText = testText + "Distance: " + dataReceived.distance + " m \n";
+      testText = testText + "Watts: " + dataReceived.watts + " w \n";
+      testText = testText + "HR: " + dataReceived.hRate + " bpm \n";
 
       noStroke();
       fill(255);
       text(testText, 10, 10, width-10, height-10);
 
       fill(150);
-      ellipse(width/2, height/2,dataReceived.logData.w, dataReceived.logData.w);
+      ellipse(width/2, height/2, dataReceived.watts, dataReceived.watts);
   }
 
 }
-
-// code to close connection if need be
-// socket.disconnect() ;
